bd0e4db5a3303f7dd323b3588d9f5c89e68afbe4
KlasL4243 <lukasklassen4243@gmail.com>
2024-08-05 12:44:37 +0200

    chore: add emulator cofig

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_list/firebase_options.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
 
   runApp(const MainApp());
 }
